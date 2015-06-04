e37c0e361363baad3ebdeed0d81fce70ea61a0a1
Ben Sima <bensima@gmail.com>
2015-06-04 15:14:39 -0400

    click!

--- burn.py
+++ burn.py
@@ -4,12 +4,24 @@
 # Run with `python burn.py`
 # I use it with Simple.com :)
 
-reservoir = input("Safe-to-Spend: ")
-burn_rate = input("Saving per day: ")
-paycheck  = input("Days until next paycheck: ")
+import click # pip install click
+from decimal import *
 
-total_burn = float(burn_rate) * float(paycheck)
+@click.command()
+@click.option('--safe', prompt="Current Safe-to-Spend Resevoir", help="Safe to Spend")
+@click.option('--burn', prompt="Your savings per day", help="Burn rate")
+@click.option('--days', prompt="Days until your next paycheck", help="Days until next paycheck")
+def burn_rate(safe, burn, days):
+    """
+    Calculates my burn rate based on current paycheck, current saving
+    amount, and days until next paycheck.
+    """
+    num  = Decimal(safe) - (Decimal(burn) * Decimal(days))
+    color = "green" if num > 0 else "red"
 
-excess = float(reservoir) - total_burn
+    msg = "You have {} ramaining this pay period!".format(click.style(str(num), fg = color))
+    click.echo(msg)
 
-print("You have {} remaining this pay period!".format(excess))
+
+if __name__ == '__main__':
+    burn_rate()
